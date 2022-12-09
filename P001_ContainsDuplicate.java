26e502ad3d80783c5482dffaffc518f7794e1809
Esraa Abdelnaser <esraaabdalnaser@gmail.com>
2022-12-09 20:10:08 +0200

    Task 1

---
+++ P001_ContainsDuplicate.java
@@ -0,0 +1,37 @@
+package problemsolving_leetcode;
+
+import java.util.*;
+
+public class P001_ContainsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> setNums = new HashSet<Integer>();
+        boolean chk = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (setNums.contains(nums[i])) {
+                chk = true;
+                break;
+            } else {
+                setNums.add(nums[i]);
+            }
+        }
+        return chk;
+
+    }
+}
+//Time complexity is O(n^2)
+/*
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+*/
